80e2f6becd20b741282c17bc3a88b1ccd999bca0
Sean <Sean.Donahue.5311@gmail.com>
2020-04-02 14:45:22 -0400

    Initial commits for a pipeline modified to run fastqc on fastq files

---
+++ fastqc/fastqc_wrapper.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+from os import fspath
+from argparse import ArgumentParser
+from subprocess import check_call
+from typing import Iterable, Tuple
+
+FASTQ_EXTENSIONS = [
+    'fastq',
+    'fastq.gz',
+]
+
+def find_fastq_files(directory: Path) -> Iterable[Path]:
+    pattern = '*.{extension}'
+    for extension in FASTQ_EXTENSIONS:
+        yield from directory.glob(pattern.format(extension=extension))
+
+def main(directory: Path):
+    command = ["fastqc"]
+    for fastq_file in find_fastq_files(directory):
+        command.append(str(fastq_file))
+
+    print('Running:', ' '.join(command))
+    check_call(command)
+
+if __name__ == '__main__':
+    p = ArgumentParser()
+    p.add_argument('directory', type=Path)
+    args = p.parse_args()
+
+    main(args.directory)
---
+++ steps/fastqc.cwl
@@ -0,0 +1,31 @@
+cwlVersion: v1.0
+class: CommandLineTool
+label: Runs fastQC on each fastq file in fastq directory
+hints:
+  DockerRequirement:
+    dockerPull: "seandonahue5311/fastqc:1.0"
+baseCommand: /opt/fastqc_wrapper.py
+
+inputs:
+  fastq_dir:
+    type: Directory
+    doc: Directory containing fastq files to be evaluated
+    inputBinding:
+      position: 1
+
+outputs:
+  zipped_files:
+    type:
+      type: array
+      items: File
+    outputBinding:
+      glob: "*.zip"
+    doc: Individual graph files and additional data files containing the raw data from which plots were drawn.
+
+  report_files:
+    type:
+      type: array
+      items: File
+    outputBinding:
+      glob: "*.html"
+    doc: HTML reports with embedded graphs
