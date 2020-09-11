664f812e27ae1e041456ae1b17faa0dcab63b0fe
Matt Ruffalo <matt.ruffalo@gmail.com>
2020-09-11 11:51:42 -0400

    Collapse N6 and DT barcodes

--- scanpy-analysis/bin/correct_snareseq_barcodes.py
+++ scanpy-analysis/bin/correct_snareseq_barcodes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
 from pathlib import Path
-from typing import Iterable, Set
+from typing import Iterable, Mapping, Set
 
 import barcodeutils as bu
 from fastq_utils import Read, fastq_reader, find_fastq_files, revcomp
@@ -13,14 +13,34 @@ UMI_SEGMENT = slice(0, 10)
 
 BARCODE_QUAL_DUMMY = 'F' * BARCODE_LENGTH * len(BARCODE_STARTS)
 
+DATA_DIR = Path(__file__).parent / 'data/snareseq'
+BARCODE_ALLOWLIST_FILE = DATA_DIR / 'barcodes.txt'
+N6_DT_MAPPING_FILE = DATA_DIR / 'R1_dTN6_pairs.txt'
+
+class KeyDefaultDict(dict):
+    def __missing__(self, key):
+        return key
+
+def read_n6_dt_mapping(n6_dt_mapping_file: Path) -> Mapping[str, str]:
+    m = KeyDefaultDict()
+
+    with open(n6_dt_mapping_file) as f:
+        for line in f:
+            pieces = line.strip().split()
+            m[pieces[1]] = pieces[0]
+
+    return m
+
 def read_barcode_allowlist(barcode_filename: Path) -> Set[str]:
-    print('Readinb barcode allowlist from', barcode_filename)
+    print('Reading barcode allowlist from', barcode_filename)
     with open(barcode_filename) as f:
         return set(f.read().split())
 
-def main(fastq_dirs: Iterable[Path], barcode_filename: Path):
+def main(fastq_dirs: Iterable[Path], barcode_filename: Path, n6_dt_mapping_file: Path):
     barcode_allowlist = read_barcode_allowlist(barcode_filename)
     correcter = bu.BarcodeCorrecter(barcode_allowlist, edit_distance=1)
+
+    n6_dt_mapping = read_n6_dt_mapping(n6_dt_mapping_file)
 
     with open('barcode_umi.fastq', 'w') as cbo, open('transcript.fastq', 'w') as tro:
         for transcript_fastq, barcode_umi_fastq in find_fastq_files(fastq_dirs, 2):
@@ -33,6 +53,7 @@ def main(fastq_dirs: Iterable[Path], barcode_filename: Path):
                 barcode_pieces = [revcomp(br.seq[s]) for s in BARCODE_SEGMENTS]
                 rc_corrected = [correcter.correct(barcode) for barcode in barcode_pieces]
                 if all(rc_corrected):
+                    corrected[2] = n6_dt_mapping[corrected[2]]
                     corrected = [revcomp(rc_bc) for rc_bc in rc_corrected]
                     usable_count += 1
                     umi_seq = br.seq[UMI_SEGMENT]
@@ -55,11 +76,8 @@ def main(fastq_dirs: Iterable[Path], barcode_filename: Path):
 if __name__ == '__main__':
     p = ArgumentParser()
     p.add_argument('fastq_dirs', type=Path, nargs='+')
-    p.add_argument('--barcode_list_file', type=Path, nargs='?')
+    p.add_argument('--barcode_list_file', type=Path, default=BARCODE_ALLOWLIST_FILE)
+    p.add_argument('--n6_dt_mapping_file', type=Path, default=N6_DT_MAPPING_FILE)
     args = p.parse_args()
 
-    if args.barcode_list_file is None:
-        this_script = Path(__file__)
-        args.barcode_list_file = this_script.parent / 'data/snareseq/barcodes.txt'
-
-    main(args.fastq_dirs, args.barcode_list_file)
+    main(args.fastq_dirs, args.barcode_list_file, args.n6_dt_mapping_file)
