49b62f737ae33865cbb238142fa92962cdd5986f
Sean <Sean.Donahue.5311@gmail.com>
2024-07-25 09:31:44 -0400

    Formatting with black and isort

--- bin/analysis/compute_qc_metrics.py
+++ bin/analysis/compute_qc_metrics.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import json
 from argparse import ArgumentParser
+from os import fspath
 from pathlib import Path
 from typing import List, Optional, Tuple
-from os import fspath
 
 import anndata
-import muon as mu
 import manhole
+import muon as mu
 import pandas as pd
 import scanpy as sc
 
@@ -47,7 +47,11 @@ def write_scanpy_qc(adata: anndata.AnnData):
 
 
 def main(assay: Assay, primary_matrix_path: Path, secondary_matrix_path: Path, salmon_dir: Path):
-    expr_primary = mu.read(f"{fspath(primary_matrix_path)}/rna") if primary_matrix_path.suffix == ".h5mu" else anndata.read_h5ad(primary_matrix_path)
+    expr_primary = (
+        mu.read(f"{fspath(primary_matrix_path)}/rna")
+        if primary_matrix_path.suffix == ".h5mu"
+        else anndata.read_h5ad(primary_matrix_path)
+    )
     if assay.secondary_analysis_layer in expr_primary.layers:
         expr_primary.X = expr_primary.layers[assay.secondary_analysis_layer]
     expr_primary.var_names_make_unique()
@@ -58,7 +62,11 @@ def main(assay: Assay, primary_matrix_path: Path, secondary_matrix_path: Path, s
     unspliced_total = expr_primary.layers["unspliced"].sum()
     spliced_unspliced_total = expr_primary.layers["spliced_unspliced_sum"].sum()
 
-    expr_secondary = mu.read(f"{fspath(secondary_matrix_path)}/rna") if secondary_matrix_path.suffix == ".h5mu" else anndata.read_h5ad(secondary_matrix_path)
+    expr_secondary = (
+        mu.read(f"{fspath(secondary_matrix_path)}/rna")
+        if secondary_matrix_path.suffix == ".h5mu"
+        else anndata.read_h5ad(secondary_matrix_path)
+    )
 
     salmon_measures = read_salmon_output(salmon_dir)
 
