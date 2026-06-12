81ca07f85c28be38c9dc53cd97527b0ca473b596
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-06-12 10:10:09 -0400

    only plot if spatial key exists

--- bin/analysis/squidpy_entry_point.py
+++ bin/analysis/squidpy_entry_point.py
@@ -127,16 +127,14 @@ def main(assay: Assay, h5ad_file: Path, img_dir: Path = None):
         output_file_stem_dict = {Assay.VISIUM_FF:"Visium", Assay.SLIDESEQ:"Slideseq"}
         output_file_stem = output_file_stem_dict[assay]
         # sdata.write(f'{output_file_stem}.zarr')
-        print(adata)
-        print(adata.uns_keys())
-        print(adata.obsm['spatial'])
 
         sq.gr.spatial_neighbors(adata)
         sq.gr.nhood_enrichment(adata, cluster_key="leiden")
 
-        with new_plot():
-            sq.pl.spatial_scatter(adata, color="leiden")
-            plt.savefig("spatial_scatter.pdf", bbox_inches="tight")
+        if 'spatial' in adata.uns.keys():
+            with new_plot():
+                sq.pl.spatial_scatter(adata, color="leiden")
+                plt.savefig("spatial_scatter.pdf", bbox_inches="tight")
 
         with new_plot():
             sq.pl.nhood_enrichment(adata, cluster_key="leiden")
